cd7fa497738d315eeb12ee0040cacec10232272b
MCBerberov19 <MCBerberov19@codingburgas.bg>
2021-10-18 22:58:54 +0300

    Create an enum and structure which will store cellData

--- Maze-Project/Maze-Project/MazeGenerationAlgorithm.h
+++ Maze-Project/Maze-Project/MazeGenerationAlgorithm.h
@@ -1,5 +1,28 @@
 #pragma once
+
+#define SIZE 22
 class MazeGenerationAlgorithm
 {
+private:
+	enum Positions {
+		ARROW_LEFT = 75,
+		ARROW_RIGHT = 77,
+		ARROW_UP = 72,
+		ARROW_DOWN = 80,
+		GRIDSYMB = 177,
+	};
+
+public:
+	struct Cell
+	{
+		bool isVisited;
+
+		bool topWall;
+		bool bottomWall;
+		bool leftWall;
+		bool rightWall;
+
+		char symb;
+	};
 };
 
