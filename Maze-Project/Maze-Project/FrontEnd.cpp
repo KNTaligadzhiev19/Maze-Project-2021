d07b46d54efa8149d1586023ffd00ee872713eff
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-10-20 18:20:13 +0300

    Add Rules table

--- Maze-Project/Maze-Project/FrontEnd.cpp
+++ Maze-Project/Maze-Project/FrontEnd.cpp
@@ -1,5 +1,7 @@
 #include "FrontEnd.h"
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 
 bool setColor(WORD newColor)
@@ -13,3 +15,44 @@ bool setColor(WORD newColor)
 
 	return false;
 }
+
+
+string printRowLine(char sign) 
+{
+	string wholeLine = "";
+	for (int i = 0; i <= 30; i++)
+	{
+		wholeLine += sign;
+	}
+	return wholeLine;
+}
+
+int main()
+{
+	char angle[4] = { char(218), char(191), char(192), char(217) };
+	char rowLine = char(196), colLine = char(179);
+
+	cout << "\n\n" << setw(33) << angle[0] << printRowLine(rowLine) << angle[1] << endl;
+	cout << setw(33) << colLine << setw(20) << "R U L E S" << setw(12) << colLine << endl;
+	cout << setw(33) << angle[2] << printRowLine(rowLine) << angle[3] << endl;
+
+	cout << setw(33) << angle[0] << printRowLine(rowLine) << angle[1] << endl;
+	cout << setw(33) << colLine << setw(26) << "X  -  Your Position" << setw(6) << colLine << endl;
+	cout << setw(33) << colLine << setw(22) << "Y  -  Your Goal" << setw(10) << colLine << endl;
+	cout << setw(33) << char(195) << printRowLine(rowLine) << char(180) << endl;
+
+	cout << setw(33) << colLine << setw(20) << "W  -  Move Up" << setw(12) << colLine << endl;
+	cout << setw(33) << colLine << setw(22) << "A  -  Move Left" << setw(10) << colLine << endl;
+	cout << setw(33) << colLine << setw(22) << "S  -  Move Down" << setw(10) << colLine << endl;
+	cout << setw(33) << colLine << setw(23) << "D  -  Move Right" << setw(9) << colLine << endl;
+	cout << setw(33) << char(195) << rowLine << rowLine << rowLine;
+	cout << setw(26) << rowLine << rowLine << rowLine << char(180) << endl;
+	cout << setw(33) << colLine << setw(25) << "You can use Buttons" << setw(7) << colLine << endl;
+	cout << setw(33) << angle[2] << printRowLine(rowLine) << angle[3] << endl;
+
+	cout << setw(33) << angle[0] << printRowLine(rowLine) << angle[1] << endl;
+	cout << setw(33) << colLine << setw(23) << "ESC - Leave Game" << setw(9) << colLine << endl;
+	cout << setw(33) << angle[2] << printRowLine(rowLine) << angle[3] << endl;
+}
+
+
