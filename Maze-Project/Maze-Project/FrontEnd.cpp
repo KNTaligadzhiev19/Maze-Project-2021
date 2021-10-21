94ac19f2f095a19e1bb87d272353bcbafc2722ed
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-10-21 21:03:25 +0300

    Create menu for Levels

--- Maze-Project/Maze-Project/FrontEnd.cpp
+++ Maze-Project/Maze-Project/FrontEnd.cpp
@@ -1,10 +1,9 @@
-```#include "FrontEnd.h"
+#include "FrontEnd.h"
 #include <iostream>
 #include <iomanip>
 #include <vector>
 #include <conio.h>
 using namespace std;
-
 
 bool setColor(WORD newColor)
 {
@@ -17,7 +16,6 @@ bool setColor(WORD newColor)
 
 	return false;
 }
-
 
 string printRowLine(char sign)
 {
@@ -57,14 +55,14 @@ void printRules()
 	cout << setw(33) << angle[2] << printRowLine(rowLine) << angle[3] << endl;
 }
 
-int returnChoice(char choice, int& position, vector<string>& form)
+int returnChoice(char choice, int& position, vector<string>& options)
 {
 
 	switch (choice)
 	{
 	case 'S': case 's': case DOWN:
 		position++;
-		if (position == form.size())
+		if (position == options.size())
 		{
 			position = 0;
 		}
@@ -73,7 +71,7 @@ int returnChoice(char choice, int& position, vector<string>& form)
 		position--;
 		if (position == -1)
 		{
-			position = form.size() - 1;
+			position = options.size() - 1;
 		}
 		break;
 	}
@@ -88,7 +86,7 @@ void printEnterChoice()
 
 void printMenu()
 {
-	vector<string> form = { " S I G N  U P ", " S I G N  I N " , " Q U I T " };
+	vector<string> options = { " S I G N  U P ", " S I G N  I N " , " Q U I T " };
 	vector<int> ident = { 22,22,19, 10,10,13 };
 	vector<int> ident2 = { 8, 8, 10, 9, 9, 12 };
 	int position = 0;
@@ -98,14 +96,14 @@ void printMenu()
 
 		system("cls");
 		cout << "\n\n\n\n\n\n" << setw(33) << char(218) << printRowLine(char(196)) << char(191) << endl;
-		for (int i = 0; i < form.size(); i++)
+		for (int i = 0; i < options.size(); i++)
 		{
 			if (position == i)
 			{
-				cout << setw(33) << char(179) << setw(ident2[i]) << char(175) << form[i] << char(174) << setw(ident2[i + 3]) << char(179) << endl;
+				cout << setw(33) << char(179) << setw(ident2[i]) << char(175) << options[i] << char(174) << setw(ident2[i + 3]) << char(179) << endl;
 			}
 			else {
-				cout << setw(33) << char(179) << setw(ident[i]) << form[i] << setw(ident[i + 3]) << char(179) << endl;
+				cout << setw(33) << char(179) << setw(ident[i]) << options[i] << setw(ident[i + 3]) << char(179) << endl;
 			}
 		}
 
@@ -114,7 +112,7 @@ void printMenu()
 
 		if (input != ENTER)
 		{
-			returnChoice(input, position, form);
+			returnChoice(input, position, options);
 		}
 		else {
 			printEnterChoice();
@@ -124,5 +122,44 @@ void printMenu()
 	}
 }
 
+void printGameMenu() {
+	vector<string> options = { " Easy Level ", " Medium Level " , " Hard Level ", " Stats ", " Rules ", " Quit " };
+	vector<int> ident = {
+		22,23,22, 19,19,18,
+		10,9,10, 13,13,14 };
 
-```
+	vector<int> ident2 = {
+		10, 9, 10, 12, 12, 12,
+		9, 8, 9, 12, 12, 13 };
+
+	int position = 0;
+
+	//Function 1
+	while (true)
+	{
+		system("cls");
+		cout << "\n\n\n\n\n\n" << setw(33) << char(218) << printRowLine(char(196)) << char(191) << endl;
+		for (int i = 0; i < options.size(); i++)
+		{
+			if (position == i)
+			{
+				cout << setw(33) << char(179) << setw(ident2[i]) << char(175) << options[i] << char(174) << setw(ident2[i + 6]) << char(179) << endl;
+			}
+			else {
+				cout << setw(33) << char(179) << setw(ident[i]) << options[i] << setw(ident[i + 6]) << char(179) << endl;
+			}
+		}
+
+		cout << setw(33) << char(192) << printRowLine(char(196)) << char(217) << endl;
+		char input = _getch();
+
+		if (input != ENTER)
+		{
+			returnChoice(input, position, options);
+		}
+		else {
+			printEnterChoice();
+			//exit(1);
+		}
+	}
+}
