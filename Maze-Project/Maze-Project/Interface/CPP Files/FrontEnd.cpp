860349f11643f0bea4a583a68bda9f4a6d6b0342
MCBerberov19 <MCBerberov19@codingburgas.bg>
2021-10-22 14:09:13 +0300

    Create case for logging into the system

--- Maze-Project/Maze-Project/Interface/CPP Files/FrontEnd.cpp
+++ Maze-Project/Maze-Project/Interface/CPP Files/FrontEnd.cpp
@@ -245,7 +245,31 @@ void printUserMenu()
 			printUserMenu();
 			break;
 		}
-		
+		case 1:
+		{
+			inputLoginAndRegisterData(*name, *password);
+			login = new LoginSystem(*name, *password);
+
+			if (login->checkLoginData())
+			{
+				delete login;
+				delete name;
+				delete password;
+				printPlayerMenu();
+			}
+			else {
+				delete login;
+				delete name;
+				delete password;
+				printUserMenu();
+			}
+			
+			break;
+		}
+		case 2:
+		{
+			exit(1);
+		}
 		
 	}
 }
