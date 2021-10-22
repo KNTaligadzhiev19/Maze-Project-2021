c420acc98ebbbbf5b1932ca4e14b82d10a2212bc
MCBerberov19 <MCBerberov19@codingburgas.bg>
2021-10-22 19:58:16 +0300

    Remove unusable file variables

--- Maze-Project/Maze-Project/Maze Configuration/Header Files/EasyLevelMaze.h
+++ Maze-Project/Maze-Project/Maze Configuration/Header Files/EasyLevelMaze.h
@@ -1,12 +1,8 @@
 #pragma once
 #include "MazeGenerationAlgorithm.h"
-#include <fstream>
 
 class EasyLevelMaze : public MazeGenerationAlgorithm
 {
-private:
-	ofstream bestStatsForEasyLevelOut;
-	ifstream bestStatsForEasyLevelIn;
 public:
 
 	EasyLevelMaze();
