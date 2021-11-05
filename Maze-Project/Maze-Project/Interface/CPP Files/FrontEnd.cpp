f0a74e837722f26c604699e75c773f933bbca792
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-11-05 19:20:49 +0200

    Add libraries for front end part

---
+++ Maze-Project/Maze-Project/Interface/CPP Files/FrontEnd.cpp
@@ -0,0 +1,8 @@
+#include "../Header Files/FrontEnd.h"
+#include "SFML/Graphics.hpp"
+#include "../../Sign System/Header Files/RegistrationSystem.h"
+#include "../../Sign System/Header Files/LoginSystem.h"
+#include "../../Sign System/Header Files/SHA256.h"
+#include "../../Maze Configuration/Header Files/EasyLevelMaze.h"
+#include "../../Maze Configuration/Header Files/MediumLevelMaze.h"
+#include "../../Maze Configuration/Header Files/HardLevelMaze.h"
--- Maze-Project/Maze-Project/Interface/Header Files/FrontEnd.h
+++ Maze-Project/Maze-Project/Interface/Header Files/FrontEnd.h
@@ -1,1 +1,2 @@
 #pragma once
+#include "SFML/Graphics.hpp"
