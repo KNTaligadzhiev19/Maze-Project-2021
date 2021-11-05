6741df41ae374d3e9077ca4bbab0215ffff586f1
MCBerberov19 <MCBerberov19@codingburgas.bg>
2021-11-05 18:39:51 +0200

    LoginSystem

--- Maze-Project/Maze-Project/Maze Configuration/CPP Files/HardLevelMaze.cpp
+++ Maze-Project/Maze-Project/Maze Configuration/CPP Files/HardLevelMaze.cpp
@@ -2,10 +2,10 @@
 
 HardLevelMaze::HardLevelMaze()
 {
-	//Set diffictuly to level 0
+	
 	level = 0;
 
-	//Setting the default pictures to print
+	
 	t1.loadFromFile("Images and Fonts/WallHard.png");
 	t2.loadFromFile("Images and Fonts/Bee.png");
 	t3.loadFromFile("Images and Fonts/Honey.png");
