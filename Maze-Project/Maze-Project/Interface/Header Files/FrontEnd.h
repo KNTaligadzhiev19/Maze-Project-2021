fb081c031aa742c7c0379abdd7410210df129612
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-11-05 19:46:48 +0200

    Add Front End Header file

--- Maze-Project/Maze-Project/Interface/Header Files/FrontEnd.h
+++ Maze-Project/Maze-Project/Interface/Header Files/FrontEnd.h
@@ -1,2 +1,28 @@
 #pragma once
 #include "SFML/Graphics.hpp"
+
+void printMenu(sf::RenderWindow& window);
+
+void onClickUserMenu(sf::RenderWindow& window, sf::Event& event1);
+
+void printLoginAndRegisterMenu(sf::RenderWindow& window);
+
+void printPlayerMenu(sf::RenderWindow& window);
+
+void onClickLoginAndRegister(sf::RenderWindow& window, sf::Event& event1);
+
+void inputLoginData(sf::RenderWindow& window, sf::Event& event1);
+
+void inputRegisterData(sf::RenderWindow& window, sf::Event& event1);
+
+void onClickPlayerMenu(sf::RenderWindow& window, sf::Event& event1);
+
+void printRulesTable(sf::RenderWindow& window);
+
+void onClickRulesTable(sf::RenderWindow& window, sf::Event& event1);
+
+void printCongratsMenu(sf::RenderWindow& window);
+
+void onClickCongratsMenu(sf::RenderWindow& window, sf::Event& event1);
+
+void setDataValue();
