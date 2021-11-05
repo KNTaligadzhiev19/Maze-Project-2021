7458a86a5400d8dd0dce155784dd004fd35f18e2
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-11-05 19:22:31 +0200

    Create function for print sfml menu

--- Maze-Project/Maze-Project/Interface/CPP Files/FrontEnd.cpp
+++ Maze-Project/Maze-Project/Interface/CPP Files/FrontEnd.cpp
@@ -26,3 +26,24 @@ namespace checkValues {
 	MediumLevelMaze* mediumMaze = NULL;
 	HardLevelMaze* hardMaze = NULL;
 };
+
+void printMenu(sf::RenderWindow& window)
+{
+	sf::Texture t1, t2, t3;
+
+	t1.loadFromFile("Login.png");
+	t2.loadFromFile("Register.png");
+	t3.loadFromFile("Logo.png");
+
+	sf::Sprite Login(t1);
+	sf::Sprite Register(t2);
+	sf::Sprite Logo(t3);
+
+	window.clear(sf::Color(0, 128, 128));
+	Logo.setPosition(190, 90);
+	window.draw(Logo);
+	Login.setPosition(300, 390);
+	window.draw(Login);
+	Register.setPosition(300, 500);
+	window.draw(Register);
+}
