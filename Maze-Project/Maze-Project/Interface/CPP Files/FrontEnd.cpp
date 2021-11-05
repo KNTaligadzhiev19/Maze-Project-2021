f58246d2025d83f47eed510209732b535c0a66da
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-11-05 19:21:51 +0200

    Create namespace for checkValues

--- Maze-Project/Maze-Project/Interface/CPP Files/FrontEnd.cpp
+++ Maze-Project/Maze-Project/Interface/CPP Files/FrontEnd.cpp
@@ -6,3 +6,23 @@
 #include "../../Maze Configuration/Header Files/EasyLevelMaze.h"
 #include "../../Maze Configuration/Header Files/MediumLevelMaze.h"
 #include "../../Maze Configuration/Header Files/HardLevelMaze.h"
+
+namespace checkValues {
+	int status = 1;
+	bool userMenuEventClick = true;
+	bool loginAndRegisterEventClick = false;
+
+	bool emailCheck = false;
+	bool passwordCheck = false;
+
+	bool logOrReg = false;
+	bool checkIn = false;
+	bool printableRulesTable = false;
+	bool inCongratsTable = false;
+	int modeChoice = 0;
+	bool inGame = false;
+
+	EasyLevelMaze* easyMaze = NULL;
+	MediumLevelMaze* mediumMaze = NULL;
+	HardLevelMaze* hardMaze = NULL;
+};
