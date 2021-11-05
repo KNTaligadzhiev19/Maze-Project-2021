7e385efe0229f0103b2ca8e8e4832fc61f871cef
MCBerberov19 <MCBerberov19@codingburgas.bg>
2021-11-05 18:44:54 +0200

    Create class for maze algorithm

--- Maze-Project/Maze-Project/Maze Configuration/Header Files/MazeGenerationAlgorithm.h
+++ Maze-Project/Maze-Project/Maze Configuration/Header Files/MazeGenerationAlgorithm.h
@@ -1,1 +1,59 @@
 #pragma once
+#include "../../Interface/Header Files/FrontEnd.h"
+
+#define HARD_SIZE 16
+
+class MazeGenerationAlgorithm
+{
+protected:
+	int positionX = 0, positionY = 0, finalPointX = 0, finalPointY = 0;
+
+	enum Positions {
+		ARROW_LEFT = 75,
+		ARROW_RIGHT = 77,
+		ARROW_UP = 72,
+		ARROW_DOWN = 80,
+		GRIDSYMB = 177,
+		BEGINSYMB = char(234),
+		ENDSYMB = char(233),
+	};
+
+	
+	struct Cell
+	{
+		bool isVisited;
+
+		bool topWall;
+		bool bottomWall;
+		bool leftWall;
+		bool rightWall;
+
+		char symb;
+	};
+
+	
+	int level = 0;
+
+	sf::Texture t1, t2, t3;
+
+public:
+
+	Cell currentCell[16][16];
+
+	char consoleGrid[16][16];
+
+	int chooseSize();
+
+	int movePixelSize();
+
+	void initializeCell();
+
+	void initializeArray();
+
+	void generateMaze();
+
+	void printMaze(sf::RenderWindow& window);
+
+	void moveOnClick(sf::RenderWindow& window, sf::Event& event1, int& check, bool& inGame, bool& congratsMenu, int& classChoice);
+};
+
