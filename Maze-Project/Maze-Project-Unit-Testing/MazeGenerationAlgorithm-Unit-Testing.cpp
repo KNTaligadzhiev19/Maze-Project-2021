c78bc87906a91cdf29e1c63faba7ea06140a9dd1
KNTaligadzhiev19 <KNTaligadzhiev19@codingburgas.bg>
2021-11-06 23:04:51 +0200

    Modify test cases

--- Maze-Project/Maze-Project-Unit-Testing/MazeGenerationAlgorithm-Unit-Testing.cpp
+++ Maze-Project/Maze-Project-Unit-Testing/MazeGenerationAlgorithm-Unit-Testing.cpp
@@ -16,7 +16,7 @@ namespace MazeProjectUnitTesting
 
 			int result = testCase.chooseSize(2);
 
-			Assert::AreEqual(result, 8);
+			Assert::AreEqual(result, 8, L"Expected 8!");
 		}
 
 		TEST_METHOD(TestingSecondOptionOfFunction_chooseSize)
@@ -25,7 +25,7 @@ namespace MazeProjectUnitTesting
 
 			int result = testCase.chooseSize(1);
 
-			Assert::AreEqual(result, 6);
+			Assert::AreEqual(result, 6, L"Expected 6!");
 		}
 
 		TEST_METHOD(TestingWithOptionThatNotExist1InFunction_chooseSize)
@@ -34,7 +34,7 @@ namespace MazeProjectUnitTesting
 
 			int result = testCase.chooseSize(0);
 
-			Assert::AreEqual(result, 0);
+			Assert::AreEqual(result, 0, L"Expected 0!");
 		}
 
 		TEST_METHOD(TestingWithOptionThatNotExist2InFunction_chooseSize)
@@ -43,7 +43,7 @@ namespace MazeProjectUnitTesting
 
 			int result = testCase.chooseSize(3);
 
-			Assert::AreEqual(result, 0);
+			Assert::AreEqual(result, 0, L"Expected 0!");
 		}
 
 		TEST_METHOD(TestingFirstOptionOfFunction_MovePixelsSize)
@@ -52,7 +52,7 @@ namespace MazeProjectUnitTesting
 
 			int result = testCase.movePixelSize(1);
 
-			Assert::AreEqual(result, 80);
+			Assert::AreEqual(result, 80, L"Expected 80!");
 		}
 
 		TEST_METHOD(TestingSecondOptionOfFunction_MovePixelsSize)
@@ -61,7 +61,7 @@ namespace MazeProjectUnitTesting
 
 			int result = testCase.movePixelSize(2);
 
-			Assert::AreEqual(result, 100);
+			Assert::AreEqual(result, 100, L"Expected 100!");
 		}
 
 		TEST_METHOD(TestingWithIncorrectDataFunction_MovePixelsSize)
@@ -70,7 +70,7 @@ namespace MazeProjectUnitTesting
 
 			int result = testCase.movePixelSize(0);
 
-			Assert::AreEqual(result, 50);
+			Assert::AreEqual(result, 50, L"Expected 50!");
 		}
 	};
 }
